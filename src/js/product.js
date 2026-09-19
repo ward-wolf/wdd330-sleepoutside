53535bffaf7a68fff8259ad4039187100794f656
ward-wolf <sward2@byupathway.edu>
2026-09-18 20:31:00 -0500

    w2 team

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,14 @@
-import { getCartItems, setLocalStorage, updateCartCount } from "./utils.mjs";
+import { getParam, updateCartCount } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-function addProductToCart(product) {
-  const cartItems = getCartItems();
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-  updateCartCount();
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
 updateCartCount();
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+
+
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,9 +1,4 @@
-// wrapper for querySelector...returns matching element
-export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
-}
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
+export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
@@ -41,4 +36,12 @@ export function setClick(selector, callback) {
     callback();
   });
   qs(selector).addEventListener("click", callback);
+}
+
+// get the product id from the query string
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
 }
